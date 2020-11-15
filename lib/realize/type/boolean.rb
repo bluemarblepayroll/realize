ea5f4da4b05804bd968cb14aabe955b491c1f509
Matthew Ruggio <mruggio@bluemarblepayroll.com>
2020-11-15 09:05:34 -0600

    Added missing code license comments

--- lib/realize/type/boolean.rb
+++ lib/realize/type/boolean.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+#
+# Copyright (c) 2020-present, Blue Marble Payroll, LLC
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
 module Realize
   class Type
     # Convert input into either true, false, or nil.
--- spec/realize/type/lowercase_string_spec.rb
+++ spec/realize/type/lowercase_string_spec.rb
@@ -2,6 +2,13 @@
 
 require 'spec_helper'
 
+#
+# Copyright (c) 2020-present, Blue Marble Payroll, LLC
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
 RSpec.describe Realize::Type::LowercaseString do
   let(:record)     { {} }
   let(:resolver)   { nil }
